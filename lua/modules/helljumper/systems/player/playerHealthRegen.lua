02d080d45fcf36d2f3561105350d5e84c4911df6
markmcfuzz <magecape10@gmail.com>
2025-08-06 02:09:41 -0600

    updated regeneration amount

--- lua/modules/helljumper/systems/player/playerHealthRegen.lua
+++ lua/modules/helljumper/systems/player/playerHealthRegen.lua
@@ -10,7 +10,7 @@ local isGameClient = function()
 end
 
 local maxHealth = 1
-local healthRegenerationAmount = 0.0037
+local healthRegenerationAmount = 0.02
 
 function playerHealthRegen.healthRegen()
     for playerIndex = 0, 15 do
@@ -27,7 +27,7 @@ function playerHealthRegen.healthRegen()
                 biped.vitals.health = 0.000000001
             end
         end
-        if biped.vitals.health < maxHealth and biped.vitals.shield > 0.95 then
+        if biped.vitals.health < maxHealth and biped.vitals.shield > 0.98 and not (biped.vehicleSeatId == 0 and biped.vehicleSeatId <= 15) then
             local newPlayerHealth = biped.vitals.health + healthRegenerationAmount
             if newPlayerHealth > 1 then
                 biped.vitals.health = 1
@@ -40,50 +40,5 @@ function playerHealthRegen.healthRegen()
         end
     end
 end
-
-
----- Lua Blam
-
---- Attempt to play a sound given tag path and optionally a gain number
--- function healthRegen.playSound(tagPath, gain)
---    local player = blam.player(get_player())
---    if player then
---        local playSoundCommand = const.hsc.playSound:format(tagPath, player.index, gain or 1.0)
---        execute_script(playSoundCommand)
---    end
--- end
-
------ Regenerate players health on low shield using game ticks
------@param playerIndex? number
---function playerHealthRegen.regenerateHealth(playerIndex)
---    -- if blam.isGameSAPP() or isGameClient() then
---    local player
---    if playerIndex then
---        player = blam.biped(get_dynamic_player(playerIndex))
---    else
---        player = blam.biped(get_dynamic_player())
---    end
---    if player then
---        -- Fix muted audio shield sync
---        -- TODO Not sure this will work as expected
---        if isGameClient() then
---            if player.health <= 0 then
---                player.health = 0.000000001
---            end
---        end
---        if player.health < 1 and player.shield >= 0.98 and blam.isNull(player.vehicleObjectId) then
---            local newPlayerHealth = player.health + const.healthRegenAiAmount
---            if newPlayerHealth > 1 then
---                player.health = 1
---                -- if isGameClient() then
---                --    healthRegen.playSound(const.sounds.humanRifleZoomIn, 5)
---                -- end
---            else
---                player.health = newPlayerHealth
---            end
---        end
---    end
---    -- end
---end
 
 return playerHealthRegen
