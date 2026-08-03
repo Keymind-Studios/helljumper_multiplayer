d564beebd3f852a0a968d48b480f487bf9534b8d
Mark <magecape10@gmail.com>
2026-08-02 23:49:59 -0700

    refactor sounds.lua to use filterTags for sound retrieval, improving reliability and consistency

--- lua/modules/helljumper/systems/constants/sounds.lua
+++ lua/modules/helljumper/systems/constants/sounds.lua
@@ -1,14 +1,17 @@
-local tagClasses = Engine.tag.classes
-local findTags = Engine.tag.findTags
-
 local sounds = {}
 
 function sounds.get()
+    -- Resolved here and not as a module upvalue: the tag API is only guaranteed to be in
+    -- place once a map is loaded, and filterTags errors if called before that.
+    local filterTags = Engine.tag.filterTags
+
     sounds.soundTag = {
-        uiGrenadePlasma = findTags("001_plasma_grenade", tagClasses.sound)[1],
-        uiGrenadeFrag = findTags("001_frag_grenade", tagClasses.sound)[1]
+        uiGrenadePlasma = filterTags("sound", "001_plasma_grenade")[1],
+        uiGrenadeFrag = filterTags("sound", "001_frag_grenade")[1],
+        humanRifleZoomIn = filterTags("sound", "007_human_rifle_zoom_in")[1],
+        humanRifleZoomOut = filterTags("sound", "007_human_rifle_zoom_out")[1]
     }
-    logger:debug("Loaded Sound tags")
+    Balltze.logger.debug("Loaded Sound tags")
 end
 
 return sounds
