e949c8cc22c116080c00d5f570d8ee7870b95488
Mark <magecape10@gmail.com>
2026-08-23 21:14:51 -0700

    add commands module

---
+++ lua/modules/helljumper/systems/debug/commands.lua
@@ -0,0 +1,25 @@
+local luna = require "luna"
+
+local commands = {}
+
+commands = {
+    debug = {
+        description = "Toggle debug mode.",
+        category = "debug",
+        help = "<boolean>",
+        example = "debug true",
+        minArgs = 1,
+        maxArgs = 1,
+        func = function(isEnabled)
+            DebugMode = luna.bool(isEnabled)
+            if DebugMode then
+                Balltze.logger.info("Debug mode enabled.")
+            else
+                Balltze.logger.info("Debug mode disabled.")
+            end
+            Balltze.logger.muteDebug(not DebugMode)
+        end
+    }
+}
+
+return commands
